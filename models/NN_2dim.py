66f553d4c888859f3205debc80c64b77dc9ce683
zykhoo <46671677+zykhoo@users.noreply.github.com>
2022-04-10 18:11:24 +0800

    Update NN_2dim.py

--- models/NN_2dim.py
+++ models/NN_2dim.py
@@ -268,7 +268,7 @@ def compute_metrics_NN(nn, h, diagdist, xshort, yshort, xlong, ylong, eval_len, 
         time_within_naive += time.time()-starttime
         MSE_within_naive += MSE(long_groundtruth[count,1,:,:steps], results_start[1,:,:], diagdist)
         starttime = time.time()
-        results_start = np.asarray(gen_one_trajNNH_autograd(eval_len,i,h,model=nn)) 
+        results_start = np.asarray(gen_one_trajNNH_autograd(steps,i,h,model=nn)) 
         time_within_leapfrog += time.time()-starttime
         MSE_within_leapfrog += MSE(long_groundtruth[count,1,:,:steps], results_start[1,:,:], diagdist)
       count+=1 
