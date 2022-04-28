0201a8f8c70edbbb894923d381c4d443ee710d69
zykhoo <46671677+zykhoo@users.noreply.github.com>
2022-04-29 00:17:35 +0800

    Update sympLeapfrog.py

--- integrators/sympLeapfrog.py
+++ integrators/sympLeapfrog.py
@@ -11,13 +11,20 @@ from tqdm import tqdm
 
 from ..metrics import MSE
 
-def LeapfrogIntNN(z,h,model):
+
+def get_grad(model, z,device):
+		inputs=Variable(torch.tensor([z[0][0],z[1][0]]), requires_grad = True).to(device)
+		out=model(inputs.float())
+		dH=torch.autograd.grad(out, inputs, grad_outputs=out.data.new(out.shape).fill_(1),create_graph=True)[0]
+		return dH.detach().cpu().numpy()[1], -dH.detach().cpu().numpy()[0] # negative dH/dq is dp/dt
+	
+def LeapfrogIntNN(z,h,model,device):
 ## classical Leapfrog scheme for force field f
 # can compute multiple initial values simultanously, z[k]=list of k-component of all initial values
 	dim = int(len(z)/2)
-	z[dim:] = z[dim:]+h/2*torch.squeeze(model(torch.tensor(z).transpose(1,0).float()),0).detach().numpy().transpose()[1]
-	z[:dim] = z[:dim]+h*torch.squeeze(model(torch.tensor(z).transpose(1,0).float()),0).detach().numpy().transpose()[0]
-	z[dim:] = z[dim:]+h/2*torch.squeeze(model(torch.tensor(z).transpose(1,0).float()),0).detach().numpy().transpose()[1]
+	z[dim:] = z[dim:]+h/2*get_grad(model, z,device)[1]
+	z[:dim] = z[:dim]+h*get_grad(model, z,device)[0]
+	z[dim:] = z[dim:]+h/2*get_grad(model, z,device)[1]
 	return z
   
 def LeapfrogTrajectoryNN(traj_len,start,h,model,n_h = 100):
@@ -26,6 +33,6 @@ def LeapfrogTrajectoryNN(traj_len,start,h,model,n_h = 100):
   for i in range(traj_len):
     start=np.hstack((start,x))
     for j in range(0,int(n_h)):
-      x=LeapfrogIntNN(x,h_gen,model)
+      x=LeapfrogIntNN(x,h_gen,model,device)
     final=np.hstack((final,x))
   return start[:,1:],final[:,1:]
