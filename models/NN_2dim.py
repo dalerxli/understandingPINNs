7ea443250c793523ade880c5263ae882aaa839f3
zykhoo <46671677+zykhoo@users.noreply.github.com>
2022-04-28 20:48:07 +0800

    Update NN_2dim.py

--- models/NN_2dim.py
+++ models/NN_2dim.py
@@ -17,52 +17,6 @@ from tqdm import tqdm
 
 from ..metrics import MSE
 
-def classicIntNN(z,h,net):
-	## classical symplectic Euler scheme
-		dim = int(len(z)/2)
-		q=z[:dim]
-		p=z[dim:]		
-		fstage = lambda stg: h * torch.squeeze(net(torch.tensor([q+stg,p]).float().transpose(1,0)),0).detach().numpy().transpose()[:dim]
-
-		stageold=np.zeros(dim) 
-		stage = fstage(stageold) +0.
-		Iter = 0
-
-		while (np.amax(abs(stage - stageold)*25) > 1e-10 and Iter<100):
-			stageold = stage+0.
-			stage = fstage(stage)+0.
-			Iter = Iter+1
-		q = q+stage
-		p = p +h*torch.squeeze(net(torch.tensor([q,p]).float().transpose(1,0)),0).detach().numpy().transpose()[dim:]
-		return np.block([q,p])
-
-def classicTrajectoryNN(z,h,net,N=1):
-	## trajectory computed with classicInt
-  z = z.reshape(1,-1)[0]
-  trj = np.zeros((len(z),N+1))
-  trj[:,0] = z.copy()
-  for j in range(0,N):
-    trj[:,j+1] = classicIntNN(trj[:,j].copy(),h,net)
-  return trj[:, :-1], trj[:, 1:]
-
-def naiveIntNN(z,h,net):
-		dim = int(len(z)/2)
-		q=z[:dim]
-		p=z[dim:]		
-		dH = torch.squeeze(net(torch.tensor(z).float()),0).detach().numpy().transpose()
-		q = q +h*dH[:dim]
-		p = p +h*dH[dim:]
-		return np.block([q,p])
-
-def naiveTrajectoryNN(z,h,net,N=1):
-	## trajectory computed with classicInt
-  z = z.reshape(1,-1)[0]
-  trj = np.zeros((len(z),N+1))
-  trj[:,0] = z.copy()
-  for j in range(0,N):
-    trj[:,j+1] = naiveIntNN(trj[:,j].copy(),h,net)
-  return trj[:, :-1], trj[:, 1:]
-
 def LeapfrogNNH_autograd(z,h,model):
 ## classical Leapfrog scheme for force field f
 # can compute multiple initial values simultanously, z[k]=list of k-component of all initial values
